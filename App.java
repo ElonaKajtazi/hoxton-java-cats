24996ae14f6abe5398fd29e6eaabb4fe8acb181e
Elona Kajtazi <MY_NAME@example.com>
2022-10-03 22:16:52 +0200

    ...

--- App.java
+++ App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main (String[] args) {
       Cat cat1 = new Cat("Oliver", 4, false);
-        cat1.displayInfo();
+      cat1.displayInfo();
       Cat cat2 = new Cat("Tom", 2, true);
       cat2.displayInfo();
 
